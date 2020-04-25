de17cc39ef7ed60b01a3b2dd9af3a0725744e58b
hems patel <patel.hemant15@gmail.com>
2020-04-25 12:50:54 +1200

    Add all files

---
+++ hemspatel-bootstrap-slider-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+Plugin Name: Bootstrap 4 Responsive multipurpose slider or carousel
+Plugin URI: http://hems.patel/
+Description: Multipurpose Bootsrap 4 Responsive slider or carousel.
+Author: Hems Patel
+Version: 1.0
+Author URI: http://hems.patel
+*/
